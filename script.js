6b9bf902bed5e1c9f0568d9c6e63b9f49f4b1f60
damondev <damonestudos@gmail.com>
2023-05-25 22:32:17 -0300

    conversor

---
+++ script.js
@@ -0,0 +1,11 @@
+var texto = document.getElementById('text')
+
+function maior(){
+        document.getElementById('text').value = texto.value.toUpperCase()
+}
+function menor(){
+        document.getElementById('text').value = texto.value.toLowerCase()
+}
+function clean(){
+            document.getElementById('text').value = ""
+}
